d6f8f3bbdce06178f5c66d1b94c7fc0346bdab00
liulka_s <asl.warmhaus@gmail.com>
2023-06-13 08:15:14 +0200

    1

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+// import css from './Statistics.module.css';
+
+// export const Statistics = ({ data }) => {
+//   return (
+//     <section className={css.statistics}>
+//       {<title />}
+//       {<stats />}
+//     </section>
+//   );
+// };
